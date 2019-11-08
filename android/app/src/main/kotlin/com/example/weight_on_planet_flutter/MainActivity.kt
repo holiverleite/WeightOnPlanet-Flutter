80299cba901c7bb1ee011dd217a2f220779946e5
Haroldo Leite <holiverleite@gmail.com>
2019-11-08 10:39:59 -0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/weight_on_planet_flutter/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.weight_on_planet_flutter
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import './ui/home.dart';
+
+// void main() => runApp(Home());
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Weight On Planet X",
+    home: new Home(),
+    )
+  );
+}
